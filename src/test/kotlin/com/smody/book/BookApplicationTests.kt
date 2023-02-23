81e1a5ca3540f7a69495f60b21ba0390b71c9391
tonic523 <diddmlvkf@gmail.com>
2023-02-23 15:14:17 +0900

    git init

---
+++ src/test/kotlin/com/smody/book/BookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smody.book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

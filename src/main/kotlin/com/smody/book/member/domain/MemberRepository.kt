2de02e822753500367e335414dcf0e3ed02e0448
does <ldk980130@gmail.com>
2023-03-03 14:35:24 +0900

    refactor: JpaRepository 대신 Repository 이용

--- src/main/kotlin/com/smody/book/member/domain/MemberRepository.kt
+++ src/main/kotlin/com/smody/book/member/domain/MemberRepository.kt
@@ -1,8 +1,12 @@
 package com.smody.book.member.domain
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.Repository
 
-interface MemberRepository: JpaRepository<Member, Long> {
+interface MemberRepository: Repository<Member, Long> {
+
+    fun save(member: Member): Member
 
     fun findByEmail(email: String): Member?
+
+    fun existsById(id: Long): Boolean
 }
